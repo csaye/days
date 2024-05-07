24a979b8bfdea7cdbfab9fa8308f5c6b93e7140b
Cooper Saye <coopersaye@gmail.com>
2024-05-06 18:33:08 -0700

    Create day counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
-import styles from '@/styles/pages/Index.module.scss';
+import styles from '@/styles/pages/Index.module.scss'
+import { useEffect, useState } from 'react'
+
+const start = {
+  activity: 'Ramp',
+  date: new Date(2023, 4, 15),
+}
 
 export default function Index() {
+  const [seconds, setSeconds] = useState(getSeconds())
+
+  const days = seconds / 60 / 60 / 24
+
+  useEffect(() => {
+    const interval = setInterval(() => setSeconds(getSeconds()), 100)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className={styles.container}>
+      <div className={styles.center}>
+        <h2>{start.activity}</h2>
+        <h1>Day {Math.floor(days)}</h1>
+        <p>{Math.round(days * 1000000) / 1000000}</p>
+      </div>
     </div>
-  );
+  )
+
+  function getSeconds() {
+    const ms = Date.now() - start.date.getTime()
+    return ms / 1000
+  }
 }
